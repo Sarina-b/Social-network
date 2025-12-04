6706584342b2b3ddce1f42b5f29a9998e9740eca
Sarina babadi <sarinababadi900@gmail.com>
2025-12-04 19:59:35 +0330

    Models are added

--- network/models.py
+++ network/models.py
@@ -1,6 +1,33 @@
+import time
+from time import timezone
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+# User(All Abstract users features)
+# Post(Post_id(primary_key),username_of_poster(foreign_key) , content , like , date_posted)
+# Profile(Profile_id(primary_key) , User_id(foreign_key , unique) , number_of_followers , number_of_followings)
+
+
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    username_of_poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=time.time())
+    like = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.username_of_poster} , {self.date_posted} , {self.like} , {self.content}"
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    number_of_followers = models.PositiveIntegerField(default=0)
+    number_of_following = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.user} , {self.number_of_followers} , {self.number_of_following}"
